02157151be9bb30144fe8a53ae937e3e7b522ad3
Prajjwal Pratap Singh <prajjwalpratap110@gmail.com>
2025-04-25 02:27:58 +0530

    Portfolio Website

--- script.js
+++ script.js
@@ -1,34 +1,3 @@
-function clutterMaking(kuchdo) {
-    var content = kuchdo.textContent;
-    var clutter = "";
-    var spiltedText = content.split("");
-    spiltedText.forEach(elem => {
-        clutter = clutter + `<span>${elem}</span>`;
-    })
-    kuchdo.innerHTML = clutter;
-}
-function locomotiveAnimation(){
-    gsap.registerPlugin(ScrollTrigger);
-  
-  const locoScroll = new LocomotiveScroll({
-  el: document.querySelector("#main"),
-  smooth: true
-  });
-  locoScroll.on("scroll", ScrollTrigger.update);
-  ScrollTrigger.scrollerProxy("#main", {
-  scrollTop(value) {
-    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-  },
-  getBoundingClientRect() {
-    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
-  },
-  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
-  });
-  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-  ScrollTrigger.refresh();
-}
-locomotiveAnimation();
-
 var tl = gsap.timeline();
 tl.from("nav .part1 h2", {
     y: -50,
@@ -46,77 +15,85 @@ tl.from("#page1 .text-part h2,#page1 .text-part hr,#page1 .text-part h1,#page1 .
     stagger: 0.1
 })
 
+function clutterMaking(kuchdo){
+    let content = kuchdo.textContent;
+    let spilted = content.split("");
+    let clutter = "";
+    spilted.forEach(elem => {
+        clutter+= `<span>${elem}</span>`
+    });
+    kuchdo.innerHTML = clutter;
+}
+
 var h2 = document.querySelector("#about .div2 h2");
 clutterMaking(h2);
-gsap.from("#about .div2 h2 span", {
+gsap.from("#about .div2 h2 span",{
     opacity: 0,
     stagger: 0.12,
-    scrollTrigger: ("#about .div2 h2 span", {
+    scrollTrigger: ("#about .div2 h2 span",{
+        scroller: "body",
         trigger: "#about .div2 h2 span",
-        scroller: "#main",
         start: "top 70%",
         end: "top 50%",
-        scrub: 1
+        scrub:1
     })
 })
-
-gsap.from("#about .div2 p", {
-    x: -30,
+gsap.from("#about .div2 p",{
+    x: 30,
     opacity: 0,
-    scrollTrigger: ("#about .div2 p", {
+    scrollTrigger: ("#about .div2 p",{
+        scroller: "body",
         trigger: "#about .div2 p",
-        scroller: "#main",
         start: "top 70%"
     })
 })
-
-var heading = document.querySelector("#page3 .div2 h2");
-clutterMaking(heading);
-gsap.from("#page3 .div2 h2 span", {
+h2 = document.querySelector("#page3 .div2 h2");
+clutterMaking(h2);
+gsap.from("#page3 .div2 h2 span",{
     opacity: 0,
-    stagger: 0.03,
-    scrollTrigger: ("#page3 .div2 h2 span", {
+    stagger: 0.12,
+    scrollTrigger: ("#page3 .div2 h2 span",{
+        scroller: "body",
         trigger: "#page3 .div2 h2 span",
-        scroller: "#main",
         start: "top 70%",
-        end: "top 50%",
-        scrub: 1
+        end: "top 90%",
+        scrub:1
     })
 })
 
-function animateLeft(content) {
-    gsap.from(content, {
+function animateLeft(content){
+    gsap.from(content,{
         x: -50,
         opacity: 0,
-        scrollTrigger: (content, {
-            scroller: "#main",
+        scrollTrigger: (content,{
+            scroller: "body",
             trigger: content,
-            start: "top 65%",
-            end: "top 75%",
+            start: "top 60%",
+            end: "top 40%",
             scrub: 1
         })
     })
 }
-function animateRight(content) {
-    gsap.from(content, {
+function animateRight(content){
+    gsap.from(content,{
         x: 50,
         opacity: 0,
-        scrollTrigger: (content, {
-            scroller: "#main",
+        scrollTrigger: (content,{
+            scroller: "body",
             trigger: content,
-            start: "top 70%",
-            end: "top 80%",
+            start: "top 60%",
+            end: "top 40%",
             scrub: 1
         })
     })
 }
 animateLeft("#project .wrapper .row .pehla");
-animateRight("#project .wrapper .row .first");
-animateLeft("#project .wrapper .row .dusra");
-animateRight("#project .wrapper .row .second");
+animateRight("#project .wrapper .row .dusra");
 animateLeft("#project .wrapper .row .teesra");
+animateRight("#project .wrapper .row .last");
+animateRight("#project .wrapper .row .first");
+animateLeft("#project .wrapper .row .second");
 animateRight("#project .wrapper .row .third");
-animateLeft("#project .wrapper .row .last");
-animateRight("#project .wrapper .row .fourth");
+animateLeft("#project .wrapper .row .fourth");
 animateLeft("#contact .left");
 animateRight("#contact .right");
